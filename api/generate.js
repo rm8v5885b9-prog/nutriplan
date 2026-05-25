9aa49f47b7c102ba0d6506fdd694e80e6726a225
rm8v5885b9-prog <rm8v5885b9@privaterelay.appleid.com>
2026-05-25 13:50:49 +0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
 3 совета по плану.`;
 
   try {
-   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
     
     const response = await fetch(url, {
       method: 'POST',
